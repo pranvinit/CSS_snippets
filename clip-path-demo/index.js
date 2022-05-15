6c7f621f417f2890a1ac2fd5bc1f0c094aa85641
Pranav <pranvofficial@gmail.com>
2022-05-15 23:25:01 +0530

    added vertical and horizontal css snap

--- clip-path-demo/index.js
+++ clip-path-demo/index.js
@@ -1,9 +1,17 @@
 const target = document.getElementById("target");
+const box = document.getElementById("box");
 const bg = document.getElementById("box-bg");
 
-target.addEventListener('mouseover', () => {
-    bg.style.clipPath = 'circle(100% at 30% 30%)';
-})
-target.addEventListener('mouseout', () => {
-    bg.style.clipPath = 'circle(0% at 0% 0%)';
-})
+const nodes = [target, box];
+
+nodes.map((e) => {
+  e.addEventListener("mouseover", () => {
+    bg.style.clipPath = "circle(100% at 30% 30%)";
+  });
+});
+
+nodes.map((e) => {
+  e.addEventListener("mouseout", () => {
+    bg.style.clipPath = "circle(0% at 0% 0%)";
+  });
+});
